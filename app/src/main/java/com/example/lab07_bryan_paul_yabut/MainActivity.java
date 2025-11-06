8c0be5891c4fa345f0f515a01c5d0a0c1c0d486f
Bryan Yabut <bryan.yabut@outlook.com>
2025-11-05 21:53:27 -0500

    added some logic in main activity

--- app/src/main/java/com/example/lab07_bryan_paul_yabut/MainActivity.java
+++ app/src/main/java/com/example/lab07_bryan_paul_yabut/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab07_bryan_paul_yabut;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_EDIT = 100;
+    private DBHelper db;
+    private ListView listView;
+    private ArrayList<Book> books;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = new DBHelper(this);
+        listView = findViewById(R.id.listViewBooks);
+        Button btnAdd = findViewById(R.id.btnAdd);
+
+        loadBooks();
+
+        btnAdd.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, EditBookActivity.class);
+            startActivityForResult(i, REQUEST_EDIT);
+        });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            Book b = books.get(position);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Delete")
+                    .setMessage("Delete \"" + b.getTitle() + "\"?")
+                    .setPositiveButton("Delete", (dialog, which) -> {
+                        db.deleteBook(b.getId());
+                        loadBooks();
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+            return true;
+        });
+    }
+
+    private void loadBooks(){
+
     }
 }
